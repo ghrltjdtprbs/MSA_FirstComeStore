eec7224ea6f0c9a66d162bbe365c3306796d55e6
hym <ghrltjdtprbs@gmail.com>
2024-08-07 15:55:47 +0900

    [feat] 프로젝트 생성
    변경 사항 :
     - 프로젝트를 생성하였습니다.

---
+++ src/main/java/com/firstcomestore/FirstComeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.firstcomestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstComeStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstComeStoreApplication.class, args);
+    }
+
+}
