362dc4f0600900c594a1f0048d51da45bae7802e
hym <ghrltjdtprbs@gmail.com>
2024-08-24 16:29:22 +0900

    [refactor] user 도메인 api restful하게 수정
    변경 사항 :
    - 기존 /user api를 더 restful 하게 /users로 변경

--- src/main/java/com/firstcomestore/domain/user/controller/EmailController.java
+++ src/main/java/com/firstcomestore/domain/user/controller/EmailController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user/email")
+@RequestMapping("/users/email")
 @RequiredArgsConstructor
 public class EmailController {
 
